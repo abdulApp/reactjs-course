712819d33ff7fff6d8117f86efd8b35579631f9f
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-09 13:39:07 +0300

    Embedding Dynamic Content

--- reactjs-course-part-1--TLB-001/src/Fundamentals/Embedding Dynamic Content/Embedding.jsx
+++ reactjs-course-part-1--TLB-001/src/Fundamentals/Embedding Dynamic Content/Embedding.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Embedding = () => {
     const greet = 'Hello, World!';
-    const date = new Date();
+    const date = new Date(); // .toLocaleDateString();
   return (
     <div>
         <h1>{greet}</h1>
