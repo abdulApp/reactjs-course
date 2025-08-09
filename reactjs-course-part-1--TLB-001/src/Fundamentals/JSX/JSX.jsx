26f3083603bc14fba5945f9e8c0fccd417295da1
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-09 12:35:59 +0300

    JSX

--- reactjs-course-part-1--TLB-001/src/Fundamentals/JSX/JSX.jsx
+++ reactjs-course-part-1--TLB-001/src/Fundamentals/JSX/JSX.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 
 const JSX = () => {
-  return (
-    <section id='section'>
-        <h1>My Website</h1>
-        <article>
-            <h2>Welcome to React</h2>
-            <p></p>
-        </article>
-    </section>
-  )
-}
+  return _jsxs("section", {
+    id: "section",
+    children: [
+      _jsx("h1", {
+        children: "My Website",
+      }),
+      _jsxs("article", {
+        children: [
+          _jsx("h2", {
+            children: "Welcome to React",
+          }),
+          _jsx("p", {
+            className: "text",
+            children: "Paragraph Content",
+          }),
+        ],
+      }),
+    ],
+  });
+};
 
-export default JSX
+export default JSX;
