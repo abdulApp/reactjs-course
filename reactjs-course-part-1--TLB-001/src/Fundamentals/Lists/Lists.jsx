025d3d894b3e1aed25ee2cfe052a4ca22866dc03
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-09 14:23:34 +0300

    lists

--- reactjs-course-part-1--TLB-001/src/Fundamentals/Lists/Lists.jsx
+++ reactjs-course-part-1--TLB-001/src/Fundamentals/Lists/Lists.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Lists = () => {
   const numbers = [1, 2, 3, 4, 5];
-  return <main>Lists</main>;
+  return <main>
+    {numbers.map((number, index) => (
+      <div key={index}>
+        Number: {number}
+      </div>
+    ))}
+  </main>;
 };
 
 export default Lists;
