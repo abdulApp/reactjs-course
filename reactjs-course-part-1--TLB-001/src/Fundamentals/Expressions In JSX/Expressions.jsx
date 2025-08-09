25fe2e6139761591f6ea5cb7b2b1ff5fb5ef3fbe
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-09 13:18:44 +0300

    Expressions In JSX

--- reactjs-course-part-1--TLB-001/src/Fundamentals/Expressions In JSX/Expressions.jsx
+++ reactjs-course-part-1--TLB-001/src/Fundamentals/Expressions In JSX/Expressions.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Expressions = () => {
+    const name = 'Abdulrahman Alamoudi';
+    const multiply = (a, b) => a * b;
   return (
     <section>
         <p>2 + 2 = {2 + 2}</p>
-        <h1></h1>
+        <h1>{name}</h1>
+        <p>2 * 2 = {multiply(2, 2)}</p>
     </section>
   )
 }
