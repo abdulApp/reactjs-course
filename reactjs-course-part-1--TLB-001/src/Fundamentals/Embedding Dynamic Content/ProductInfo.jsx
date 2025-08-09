c550962690a1d94e5826f2a1c11d9f900ecfb88b
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-09 13:42:21 +0300

    Embedding Dynamic Content

--- reactjs-course-part-1--TLB-001/src/Fundamentals/Embedding Dynamic Content/ProductInfo.jsx
+++ reactjs-course-part-1--TLB-001/src/Fundamentals/Embedding Dynamic Content/ProductInfo.jsx
@@ -15,6 +15,7 @@ const ProductInfo = () => {
         <h1>{product.name}</h1>
         <p>Price: ${product.price.toFixed(2)}</p>
         <p>{product.inStock ? 'In Stock' : 'Out of Stock'}</p>
+        <p>{product.description}</p>
     </div>
   )
 }
