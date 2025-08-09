1ff8e28bf73ec25195cc175d6678fe8070c84f91
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-09 19:43:48 +0300

    Props Children

--- reactjs-course-part-1--TLB-001/src/Fundamentals/PropsChildren/PropsChildren.jsx
+++ reactjs-course-part-1--TLB-001/src/Fundamentals/PropsChildren/PropsChildren.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const PropsChildren = ({children}) => {
-  return (
-    <div>{children}</div>
-  )
-}
+const PropsChildren = ({ children }) => {
+  return <div>{children}</div>;
+};
 
-export default PropsChildren
+export default PropsChildren;
