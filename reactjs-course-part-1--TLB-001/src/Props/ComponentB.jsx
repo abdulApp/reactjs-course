0e3a1808de98c690f9eedee287f311ac83bf2324
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-23 01:14:07 +0300

    context api

--- reactjs-course-part-1--TLB-001/src/Props/ComponentB.jsx
+++ reactjs-course-part-1--TLB-001/src/Props/ComponentB.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Data } from '../ContextAPI/Context';
+import React from "react";
+import { Data } from "../ContextAPI/Context";
 
 const ComponentB = () => {
   return (
     <Data.Consumer>
-      
+      {(name) => {
+        return <h1>Hello {name}, Welcome to Context API</h1>;
+      }}
     </Data.Consumer>
-  )
-}
+  );
+};
 
-export default ComponentB
+export default ComponentB;
