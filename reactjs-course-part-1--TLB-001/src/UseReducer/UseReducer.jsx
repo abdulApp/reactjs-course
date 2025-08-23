f72702d35909ee3b0b728b694221f5f1aa804517
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-23 13:52:27 +0300

    useReducer

--- reactjs-course-part-1--TLB-001/src/UseReducer/UseReducer.jsx
+++ reactjs-course-part-1--TLB-001/src/UseReducer/UseReducer.jsx
@@ -14,7 +14,7 @@ const initialState = { count: 0 };
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 };
     case "decrement":
       return { count: state.count - 1 };
     default:
