e72db69822509be407460e2875d784fb1e3354a1
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2025-08-23 13:19:42 +0300

    useReducer

--- reactjs-course-part-1--TLB-001/src/UseReducer/UseReducer.jsx
+++ reactjs-course-part-1--TLB-001/src/UseReducer/UseReducer.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useReducer } from "react";
+
+// A function you call to send actions | This is the starting value of state when
+// to the reducer, which then update   | the component is first rendered
+// the state.                          |
+//                 👇                                👇
+// const [state, dispatch] = useReducer(reducer, initialState);
+//         👆                             👆
+// The current state value which you   | This is a function that describes how the state should change
+// can use in your component           | based on the action. It takes the current state and an action
+//                                     | as inputs, and returns the new state.
+
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 const UseReducer = () => {
-    // A function you call to send actions | This is the starting value of state when 
-    // to the reducer, which then update   | the component is first rendered
-    // the state.                          |
-    //                 👇                                👇
-    // const [state, dispatch] = useReducer(reducer, initialState);
-    //         👆                             👆
-    // The current state value which you   | This is a function that describes how the state should change
-    // can use in your component           | based on the action. It takes the current state and an action
-    //                                     | as inputs, and returns the new state.
-  return (
-    <div>UseReducer</div>
-  )
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return <div>UseReducer</div>;
+};
 
-export default UseReducer
+export default UseReducer;
